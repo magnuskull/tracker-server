1c44deb6a47ab6ca82762fbe389ef5c5ee599839
Magnus Kull <magnus.kull@ub.gu.se>
2016-11-19 20:25:36 +0100

    include created_at as attribute in api

--- app/resources/v1/base_resource.rb
+++ app/resources/v1/base_resource.rb
@@ -2,15 +2,17 @@ module V1
   class BaseResource < JSONAPI::Resource
     abstract
 
+    attribute :created_at
+
     class << self
       def creatable_fields(context)
-        super - [:id]
+        super - [:created_at, :id]
       end
       alias_method :updatable_fields, :creatable_fields
     end
 
     def fetchable_fields
-      super + [:created_at, :updated_at]
+      super + [:updated_at]
     end
   end
 end
